b6256d3a1c0ec1fdd426e3862e95ef01b7beb4f0
uttank <park.yongho@gmail.com>
2025-07-26 13:23:41 +0900

    Refactor API key handling to load from environment variables

--- ch04/ch04_whisper_exp.py
+++ ch04/ch04_whisper_exp.py
@@ -1,7 +1,12 @@
 from openai import OpenAI	
-
+import os
+from dotenv import load_dotenv
+# This script loads environment variables from a .env file
+load_dotenv()
+# os.getenv() 함수를 이용해 API 키를 변수에 저장합니다.
+openai_api_key = os.getenv("OPENAI_API_KEY")
 # API 키 입력
-client = OpenAI(api_key="여러분들의 Key 값")
+client = OpenAI(api_key=openai_api_key)
 
 # 녹음 파일 열기
 audio_file = open("speech.mp3", "rb")
